64c01c982b16edf275248a2a38d7d818bf09dea9
Brito Mateus <mateusvsbrito6@gmail.com>
2023-12-11 13:31:21 -0300

    feat: controllers index

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,2 +1,6 @@
 export * from './users';
 export * from './userTypes';
+export * from './occupations';
+export * from './responsible';
+export * from './students';
+export * from './studentReponsibles';
